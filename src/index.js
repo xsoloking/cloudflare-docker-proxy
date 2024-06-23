b3090a94c366c0b1693ec26dfb0f77421c7f4133
xsoloking <soloking.x@gmail.com>
2024-06-24 06:59:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.byai.uk": dockerHub,
+  "quay.byai.uk": "https://quay.io",
+  "gcr.byai.uk": "https://gcr.io",
+  "k8s-gcr.byai.uk": "https://k8s.gcr.io",
+  "k8s.byai.uk": "https://registry.k8s.io",
+  "ghcr.byai.uk": "https://ghcr.io",
+  "cloudsmith.byai.uk": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.byai.uk": dockerHub,
 };
 
 function routeByHosts(host) {
